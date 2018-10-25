2e6e6a4d1d42606bbea043a62c39274892d1082b
maddhruv <dhruvjainpenny@gmail.com>
2018-10-25 23:09:25 +0530

    website/demo - next app

---
+++ pages/demos/button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../../lib/'
+
+export default class ButtonDemo extends React.Component {
+  render () {
+    return (
+      <div>
+        <Button>Hello</Button>
+      </div>
+    )
+  }
+}
